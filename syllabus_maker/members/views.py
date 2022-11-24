e7307d3da2f9e617bdca5267f5ecd9529df070ff
ejaybelen <qepbelen@tip.edu.ph>
2022-11-24 23:28:35 +0800

    Login system added

---
+++ syllabus_maker/members/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('list-syllabus')
+        else:
+            messages.success(request, ("There was an error logging in, try again."))
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You were logged out."))
+    return redirect('login')
+
+
